1471c83483bc9f346f33361d9926970b859a95e7
Vanyahilevich <40438281+Vanyahilevich@users.noreply.github.com>
2022-09-21 00:19:19 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // document.querySelector('#myVideo').playbackRate = 0.8;
+
+
+const button =  document.querySelector('.navigation')
+const menu =  document.querySelector('.menu')
+const menuToggle = document.querySelector('.menuToggle')
+
+menuToggle.classList.add('active__menuToggle')
+button.addEventListener("click", (e) =>{
+        menu.classList.toggle('active__menu');
+        menuToggle.classList.toggle('active__menuToggle');
+})
