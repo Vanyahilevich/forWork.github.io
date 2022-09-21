597d204874de0ae1db40ce093818f612bab56a02
Vanyahilevich <40438281+Vanyahilevich@users.noreply.github.com>
2022-09-21 18:17:48 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,8 +5,29 @@ const button =  document.querySelector('.navigation')
 const menu =  document.querySelector('.menu')
 const menuToggle = document.querySelector('.menuToggle')
 
-menuToggle.classList.add('active__menuToggle')
+menuToggle.classList.remove('active__menuToggle')
+menu.classList.add('active__menu');
 button.addEventListener("click", (e) =>{
         menu.classList.toggle('active__menu');
         menuToggle.classList.toggle('active__menuToggle');
 })
+
+
+const accordion = [...document.querySelectorAll('.vacancy__title')]
+const showAccordion = [...document.querySelectorAll('.vacancy__show')]
+const arrows = [...document.querySelectorAll('.arrow')]
+
+
+console.log(accordion)
+console.log(showAccordion)
+
+accordion.map((el,index) => {
+        el.addEventListener('click', () => {
+                showAccordion[index].classList.toggle('vacancy__showAct')
+                arrows[index].classList.toggle('arrow__act')
+        })
+})
+
+setInterval((e) => {
+console.log('3213')
+}, 500)
