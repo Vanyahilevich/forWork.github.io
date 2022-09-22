cba4ca4f96d6c0a892bd83ded6df46bfe00354f3
Vanyahilevich <40438281+Vanyahilevich@users.noreply.github.com>
2022-09-22 18:25:54 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-// document.querySelector('#myVideo').playbackRate = 0.8;
+import i18Obj from './translate.js'
 
 
+//-----------------------------------------------------------------------------burger
 const button =  document.querySelector('.navigation')
 const menu =  document.querySelector('.menu')
 const menuToggle = document.querySelector('.menuToggle')
 
 menuToggle.classList.remove('active__menuToggle')
-menu.classList.add('active__menu');
 button.addEventListener("click", (e) =>{
         menu.classList.toggle('active__menu');
         menuToggle.classList.toggle('active__menuToggle');
 })
 
-
+//-----------------------------------------------------------------------------accordion
 const accordion = [...document.querySelectorAll('.vacancy__title')]
 const showAccordion = [...document.querySelectorAll('.vacancy__show')]
 const arrows = [...document.querySelectorAll('.arrow')]
-
-
-console.log(accordion)
-console.log(showAccordion)
 
 accordion.map((el,index) => {
         el.addEventListener('click', () => {
@@ -27,7 +23,31 @@ accordion.map((el,index) => {
                 arrows[index].classList.toggle('arrow__act')
         })
 })
+//-----------------------------------------------------------------------------translate
+const getTranslate = language => {
+        let dataTranslate = document.querySelectorAll("[data-i18]");
+        dataTranslate.forEach(item => {
+                if (!item.dataset.i18) return;
+                if (item.placeholder) {
+                        item.placeholder = i18Obj[language][item.dataset.i18]
+                } else {
+                        item.textContent = i18Obj[language][item.dataset.i18]
+                }
 
-setInterval((e) => {
-console.log('3213')
-}, 500)
+        })
+
+}
+getTranslate("EN")
+
+const pressButtonLang = () => {
+        const langBtns = document.querySelector('.header__lang')
+        langBtns.addEventListener("click", (event) => {
+                if (!(event.target.classList.contains("header__btn"))) return;
+                Array.from(langBtns.children).forEach(item => item.classList.remove('active__lang'))
+                event.target.classList.add('active__lang')
+                //берем язык с нажатой кнопки
+                let lang = document.querySelector('.active__lang').innerHTML
+                getTranslate(lang)
+        })
+}
+pressButtonLang()
